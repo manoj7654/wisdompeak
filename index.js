573f24b136c17f6ac5d84221008e46055a0857f9
Manoj Kumar <manojsfstm5@gmail.com>
2024-09-28 15:16:26 +0530

    backend complete for real state project

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ const seriesRouter = require("./router/seriesRouter");
 app.use(cors())
 app.use(express.json());
 
-app.use("/",(req,res)=>{
-    res.json({message:"Welcome to the Real State Management API"})
-})
 
 app.use("/api",developerRouter)
 app.use("/api",projectRouter)
